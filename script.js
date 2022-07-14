794fbe542a5b0756ebb60c3cb15ee08ae0aaa7a7
bertrainn <pokeflux@gmail.com>
2022-07-14 14:07:32 +0800

    added basic funtionality to the calc

--- script.js
+++ script.js
@@ -57,9 +57,37 @@ class Calculator {
         this.previousOperand = ''
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+
+        let integerDisplay
+        
+        if(isNaN(integerDigits)) {
+            integerDisplay = ''
+        }
+        else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0 })
+        }
+
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        }
+        else {
+            return integerDisplay;
+        }
+    }
+
     updateDisplay() {
-        this.current_txt.innerText = this.currentOperand
-        this.previous_txt.innerText = this.previousOperand
+        this.current_txt.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null) {
+            this.previous_txt.innerText = `${this.previousOperand} ${this.operation}`
+        }
+        else {
+            this.previous_txt.innerText = ''
+        }
     }
 }
 
